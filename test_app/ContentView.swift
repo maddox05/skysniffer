b5bd9e9dd132d3fa154463f7fab6df90100b2428
maddox05 <maddoxschmidlkofer@gmail.com>
2025-12-01 18:48:21 -0500

    Initial Commit

---
+++ test_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_app
+//
+//  Created by Maddox Schmidlkofer on 12/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test_app
+//
+//  Created by Maddox Schmidlkofer on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
